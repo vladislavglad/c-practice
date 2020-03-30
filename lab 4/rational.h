b11dc8b1c5c22494e779a07ec29ec3eba3507403
vladislavglad <vladglad.996@gmail.com>
2020-03-30 14:04:58 -0400

    Header files for Rational class.

---
+++ lab 4/rational.h
@@ -0,0 +1,38 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+class Rational {
+    private:
+        int num;
+        int denom;
+    public:
+        Rational(int num, int denom);
+        Rational(int num);
+        Rational();
+        Rational(const Rational &other);
+
+        Rational neg() const;
+        Rational inv() const;
+
+        int getNumerator() const;
+        int getDenominator() const;
+
+        Rational add(const Rational &other) const;
+        Rational sub(const Rational &other) const;
+        Rational mul(const Rational &other) const;
+        Rational div(const Rational &other) const;
+
+        Rational &addInPlace(const Rational &other);
+        Rational &subInPlace(const Rational &other);
+        Rational &mulInPlace(const Rational &other);
+        Rational &divInPlace(const Rational &other);
+
+        int compareTo(const Rational &other) const;
+        bool equals(const Rational &other) const;
+
+        void print(std::ostream &os) const;
+};
+
+inline std::ostream &operator <<(std::ostream &os, const Rational &r) {r.print(os);}
+
+#endif
